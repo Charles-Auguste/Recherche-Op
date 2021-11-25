105813f6dbd3925a58209412fa2b95b77f441943
Charles-Auguste <charlesaugustegourio@gmail.com>
2021-11-25 14:57:30 +0100

    instances

--- main.py
+++ main.py
@@ -2,6 +2,36 @@ import pulp
 import json
 import json_reader as jr
 
+
 if __name__=='__main__':
-    print ("hello world !")
+
+    file_name = "KIRO-tiny.json"
+    parameters, clients, sites, siteSiteDistances, siteClientDistances = jr.read_data(file_name)
+
+    # Structure de x
+    # [ (production)[liste de 0 et de 1 de taille nb_de_sites],
+    #   (distribution)[liste de 0 et de 1 de taille nb_de_sites],
+    #   (automat)[liste de 0 et de 1 de taille nb_de_sites],
+    #   (parent)[liste d'id de sites de prod de taille nb_sites],
+    #   (client)[liste d'id de sites de taille nb_client]
+
+
+    def building_cost (s,x) :
+        pass
+
+    def production_cost (i,x) :
+        pass
+
+    def routing_cost (i,x) :
+        pass
+
+    def capacity_cost (s,x) :
+        pass
+
+
+
+
+
+
+
 
