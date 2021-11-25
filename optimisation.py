891a0c047fa05357be7f93eb8e8fdacf7a0a4e90
Michel Sénégas <michel.senegas@eleves.enpc.fr>
2021-11-25 18:23:51 +0100

    generer

--- optimisation.py
+++ optimisation.py
@@ -1,8 +1,30 @@
 import numpy as np
 
-def genere_sol_admissible(parameters,clients,sites,sitrSiteDistances,siteClientDistances):
+def genere_sol_admissible(parameters,clients,sites,siteSiteDistances,siteClientDistances):
+    nb_client = len(clients)
+    nb_site = len(sites)
     x = [[0 for i in range(nb_site)], [0 for i in range(nb_site)], [0 for i in range(nb_site)],
          [0 for i in range(nb_site)], [0 for i in range(nb_client)]]
+    for i in range(len(x[0])):
+        r=np.random.randint(-1,2)
+        if r>=0:
+            x[r][i]=1
+            if r==0:
+                q=np.random.randint(0,2)
+                if q:
+                    x[2][i]=1
+    for i in range(len(x[0])):
+        r = np.random.randint(0,len(x[0]))
+        while not x[0][r]:
+            r = np.random.randint(0,len(x[0]))
+        x[3][i]=r
+    for i in range(len(x[4])):
+        r = np.random.randint(0,len(x[0]))
+        while (not x[0][r]) and (not x[1][r]):
+            r = np.random.randint(0,len(x[0]))
+        x[4][i]=r
+    return x
+
 
 def heuristique1(parameters, clients, sites, siteSiteDistances, siteClientDistances):
     nb_client = len(clients)
