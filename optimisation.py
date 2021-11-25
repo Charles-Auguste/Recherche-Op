985078f23ad00921621fbc7db173d44e2675687c
Thomas Risola <thomas.risola@gmail.com>
2021-11-25 17:47:37 +0100

    parenthese

--- optimisation.py
+++ optimisation.py
@@ -2,18 +2,24 @@ import numpy as np
 
 
 def heuristique1(parameters, clients, sites, siteSiteDistances, siteClientDistances):
-    print("nb_clients = ", nb_client)
-    print("nb_sites = ", nb_site)
+    nb_client = len(clients)
+    nb_site = len(sites)
+    x = [[0 for i in range(nb_site)],[0 for i in range(nb_site)], [0 for i in range(nb_site)], [0 for i in range(nb_site)], [0 for i in range(nb_client)]]
     total_demand = 0
     for client in clients:
         demand = client["demand"]
         total_demand += demand
     min = 100000000000000000
     number_of_usine = total_demand / parameters["capacities"]["productionCenter"]
-    for j in range(nb_site):
-        # if la distance moyenne d'un site est plus petite que la moyenne on prend ce site
-        dist_moy = np.abs(np.mean(siteClientDistances[:, j]))
-        if (dist_moy < min):
-            min = dist_moy
-            j_choice_to_construct = j
+    j_used = []
+    for i in range(number_of_usine):
+        for j in range(nb_site):
+            # if la distance moyenne d'un site est plus petite que la moyenne on prend ce site
+            dist_moy = np.abs(np.mean(siteClientDistances[:, j]))
+            if (dist_moy < min):
+                min = dist_moy
+                if(j not in j_used):
+                    j_choice_to_construct = j
+                    j_used.append(j)
+    x[0][j]
     return 0
