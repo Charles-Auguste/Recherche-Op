03a2fc27eee6882bcdba73ac7d89931808a3cdda
Thomas Risola <thomas.risola@gmail.com>
2021-11-25 15:36:59 +0100

    production cost

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__=='__main__':
     #   (distribution)[liste de 0 et de 1 de taille nb_de_sites],
     #   (automat)[liste de 0 et de 1 de taille nb_de_sites],
     #   (parent)[liste d'id de sites de prod de taille nb_sites],
-    #   (client)[liste d'id de sites de taille nb_client]
+    #   (parent des clients)[liste d'id de sites de taille nb_client]
 
     # Contraintes sur la structure de x
 
@@ -33,7 +33,22 @@ if __name__=='__main__':
         pass
 
     def production_cost (i,x) :
-        pass
+        """i est le numero du client """
+        # if parent = producter
+        if(x[0][x[5][i-1]] == 1):
+            # i+1 car les id start a 0 et nos listes a 0
+            # x[3][x[5][i-1]] producter automatise?
+            auto = x[3][x[5][i-1]]
+            cost = clients[i-1]["demand"]*parameters["productionCosts"]["productionCenter"]-auto*parameters["productionCosts"]["automationBonus"]
+            return cost
+        # if parent = distrib
+        elif(x[1][x[5][i-1]] == 1):
+            # x[0][x[4][x[5][i-1]]] parent du distrib automatise?
+            auto = x[0][x[4][x[5][i-1]]]
+            cost = clients[i - 1]["demand"] * parameters["productionCosts"]["productionCenter"] - auto * parameters["productionCosts"]["automationBonus"] + parameters["productionCosts"]["distributionCenter"]
+            return cost
+        else:
+            return 0
 
     def routing_cost (i,x) :
         pass
