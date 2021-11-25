cd8e074bb032ba4ba2cbf8faebecc9beb5106564
Thomas Risola <thomas.risola@gmail.com>
2021-11-25 14:32:48 +0100

    dict

--- json_reader.py
+++ json_reader.py
@@ -1,5 +1,15 @@
 import json
 
-with open('data.json') as json_data:
-    data_dict = json.load(json_data)
-    print(data_dict)
+def read_data(file_name):
+    with open(file_name) as json_data:
+        data_dict = json.load(json_data)
+    param_dict = data_dict["parameters"]
+    clients_dict = data_dict["clients"]
+    sites_dict = data_dict["sites"]
+    siteSiteDistances_dict = data_dict["siteSiteDistances"]
+    siteClientDistances_dict = data_dict["siteClientDistances"]
+
+    return param_dict,clients_dict,sites_dict, siteSiteDistances_dict, siteClientDistances_dict
+
+
+
