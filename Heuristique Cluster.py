6d1ed0f21d6480ae7c8747e4df1cc2c05cf28e3d
Thomas Risola <thomas.risola@gmail.com>
2021-12-16 01:50:55 +0100

    heuristique de cluster fonctionnelle

--- Heuristique Cluster.py
+++ Heuristique Cluster.py
@@ -32,7 +32,10 @@ print("nb_clients = ", nb_client)
 print("nb_sites = ", nb_site)
 
 # Le nombre de cluster dépend de l'instance
-nb_cluster = 6  # idee int(nb_site/4)
+if file == "KIRO-large":
+    nb_cluster = 6  # idee max 10
+else:
+    nb_cluster = 3
 print("nb_clusters = ", nb_cluster)
 
 
@@ -76,21 +79,42 @@ def clustering(number_of_cluster: int, liste_client_coord: list, liste_site_coor
         affinity="nearest_neighbors",
     )
     spectral.fit(list_all_coord)
-    return spectral.labels_.astype(int)
+    predictions = spectral.labels_.astype(int)
+    predictions_client = predictions[:nb_client]
+    predictions_site = predictions[nb_client:]
+    count = np.unique(predictions_site, return_counts=True)[1]
+    for label in np.unique(predictions_site):
+        if count[label] > 10:
+            slice_index_client = []
+            slice_index_site = []
+            for i in range(len(predictions_client)):
+                if predictions[i] == label:
+                    slice_index_client.append(i)
+            for i in range(len(predictions_site)):
+                if predictions[nb_client + i] == label:
+                    slice_index_site.append(i)
+            numpy_clients = np.asarray(clients)
+            numpy_sites = np.asarray(sites)
+            sub_list_client = numpy_clients[slice_index_client]
+            sub_list_site = numpy_sites[slice_index_site]
+            sub_prediction = sub_clustering(4, get_liste_coord(sub_list_client), get_liste_coord(sub_list_site),
+                                            nb_cluster)
+            for i in range(len(slice_index_client)):
+                predictions[slice_index_client[i]] = sub_prediction[i]
+            for i in range(len(slice_index_site)):
+                predictions[nb_client + slice_index_site[i]] = sub_prediction[i + len(slice_index_client)]
+    return predictions
 
 
 # On affine la clusterisation
-def sub_clustering(number_of_sub_cluster: int, liste_client_coord: list, prediction_label: int, predictions: list):
-    spectral = cluster.SpectralClustering(
-        n_clusters=number_of_sub_cluster,
-        eigen_solver="arpack",
-        affinity="nearest_neighbors",
-    )
-
-    spectral.fit(liste_client_coord)
-
-    sub_prediction = spectral.labels_.astype(int) + len(np.unique(predictions))
-    return spectral.labels_.astype(int)
+def sub_clustering(number_of_sub_cluster: int, sub_liste_client_coord: list, sub_liste_site_coord: list,
+                   number_of_cluster: int):
+    list_all_coord = sub_liste_client_coord + sub_liste_site_coord
+    kmeans = cluster.MiniBatchKMeans(n_clusters=number_of_sub_cluster)
+    kmeans.fit(list_all_coord)
+    sub_predictions = kmeans.labels_.astype(int) + number_of_cluster
+    # print(np.unique(sub_predictions))
+    return sub_predictions
 
 
 def show_client_site(name):
@@ -144,7 +168,7 @@ def show_client_site_clustered(name, predictions: list):
 
 
 def print_sub_problem_stat(predictions, list_sub_client, list_sub_site):
-    for label in np.unique(predictions):
+    for label in range(len(np.unique(predictions))):
         print("taille du sous problème " + str(label) + ":")
         print("nb_sub_client:", len(list_sub_client[label]))
         print("nb_sub_site", len(list_sub_site[label]))
@@ -163,6 +187,8 @@ def get_sub_problem(predictions: list):
     list_sub_siteClientDistance = []
     predictions_client = predictions[:nb_client]
     predictions_site = predictions[nb_client:]
+    list_slice_index_client = []
+    list_slice_index_site = []
     for label in np.unique(predictions):
         slice_index_client = []
         slice_index_site = []
@@ -176,7 +202,46 @@ def get_sub_problem(predictions: list):
         list_sub_site.append(numpy_sites[slice_index_site])
         list_sub_siteSiteDistance.append(numpy_siteSiteDistances[slice_index_site, :][:, slice_index_site])
         list_sub_siteClientDistance.append(numpy_siteClientDistances[slice_index_site, :][:, slice_index_client])
-    return list_sub_client, list_sub_site, list_sub_siteSiteDistance, list_sub_siteClientDistance
+        # sauvegarde des indices des sous problèmes pour reconstruire la solution finale
+        list_slice_index_client.append(slice_index_client)
+        list_slice_index_site.append(slice_index_site)
+    return list_sub_client, list_sub_site, list_sub_siteSiteDistance, list_sub_siteClientDistance, list_slice_index_client, list_slice_index_site
+
+
+def solution_heuristique(parameters, list_sub_client, list_sub_site, list_sub_siteSiteDistance,
+                         list_sub_siteClientDistance,
+                         list_index_clients, list_index_sites):
+    subX = []
+    for subproblem in range(len(list_sub_clients)):
+        subX.append(pl.solution_pl(parameters, list_sub_client[subproblem], list_sub_site[subproblem],
+                                   list_sub_siteSiteDistance[subproblem],
+                                   list_sub_siteClientDistance[subproblem]))
+    return solution_reconstruction(subX, list_index_clients, list_index_sites)
+
+
+def solution_reconstruction(subX, list_id_client, list_id_site):
+    x_production = [0 for i in range(nb_site)]
+    x_distribution = [0 for i in range(nb_site)]
+    x_auto = [0 for i in range(nb_site)]
+    x_parent = [[0 for i in range(nb_site)] for j in range(nb_site)]
+    x_client = [[0 for i in range(nb_site)] for j in range(nb_client)]
+    for subproblem in range(len(subX)):
+        for site in range(len(subX[subproblem][0])):
+            x_production[list_id_site[subproblem][site]] = subX[subproblem][0][site]
+            x_distribution[list_id_site[subproblem][site]] = subX[subproblem][1][site]
+            x_auto[list_id_site[subproblem][site]] = subX[subproblem][2][site]
+        for site1 in range(len(subX[subproblem][0])):
+            for site2 in range(len(subX[subproblem][0])):
+                x_parent[list_id_site[subproblem][site1]][list_id_site[subproblem][site2]] = \
+                    subX[subproblem][3][site1][site2]
+        for site in range(len(subX[subproblem][0])):
+            # je suis pas sur du 0
+            # print("site", len(subX[subproblem][0]))
+            # print("client", len(subX[subproblem][4]))
+            for client in range(len(subX[subproblem][4])):
+                x_client[list_id_client[subproblem][client]][list_id_site[subproblem][site]] = \
+                    subX[subproblem][4][client][site]
+    return [x_production, x_distribution, x_auto, x_parent, x_client]
 
 
 if __name__ == "__main__":
@@ -185,17 +250,16 @@ if __name__ == "__main__":
     list_site_coord = get_liste_coord(sites)
     prediction = clustering(nb_cluster, list_client_coord, list_site_coord)
 
-    list_sub_clients, list_sub_sites, list_sub_siteSiteDistances, list_sub_siteClientDistances = get_sub_problem(
+    list_sub_clients, list_sub_sites, list_sub_siteSiteDistances, list_sub_siteClientDistances, list_index_client, list_index_site = get_sub_problem(
         prediction)
     # show_client_site(name_dir)
-    show_client_site_clustered(name_dir, prediction)
+    # show_client_site_clustered(name_dir, prediction)
 
     print_sub_problem_stat(prediction, list_sub_clients, list_sub_sites)
     # show_super_client(set_super_client,name_dir)
-    X = pl.solution_pl(parameters, list_sub_clients[0], list_sub_sites[0], list_sub_siteSiteDistances[0],
-                       list_sub_siteClientDistances[0])
-    # jr.write_data(jr.encode_x(X),file_name_sol_path)
+    X = solution_heuristique(parameters, list_sub_clients, list_sub_sites, list_sub_siteSiteDistances,
+                             list_sub_siteClientDistances, list_index_client, list_index_site)
+    jr.write_data(jr.encode_x(X),file_name_sol_path)
 
     print("Coût de la solution optimale : ")
-    print(int(co.total_cost(X, parameters, list_sub_clients[0], list_sub_sites[0], list_sub_siteSiteDistances[0],
-                            list_sub_siteClientDistances[0]) / 10000))
+    print(int(co.total_cost(X, parameters, clients, sites, siteSiteDistances, siteClientDistances) / 10000))
